8769d1248111d1a10c594a96711b9268f7085a03
chenux <cheche@CheMac.local>
2017-01-23 21:52:47 -0600

    None

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
 
 var compression = require('compression')
 var express = require('express');
-var express = require('http');
-//var browserify = require('browserify');
-//var React = require('react');
-//var ReactDOM = require('react-dom/server');
-
-//var jsx = require('node-jsx');
-
-
 var app = express();
-
-
-//jsx.install();
-
-var Home = require('./src/home.jsx');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.use(compression());
+
+// app.get('/', function (req, res) {
+
+// });
+
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.end(
 
-  	ReactDOM.renderToStaticMarkup(
-  		React.createElement(Home)
-  	)
-
-  );
-});
-
-
-var server = app.listen(3000, function() {
+server.listen(3000, function() {
 	var addr = server.address();
 	console.log('Listening @ http://%s:%d', addr.address, addr.port);
 })
