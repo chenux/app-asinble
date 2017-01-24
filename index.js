abbc1576d86498e18b606f5b4396233f45b97527
chenux <cheche@CheMac.local>
2017-01-23 19:05:11 -0600

    Asinble

---
+++ index.js
@@ -0,0 +1,35 @@
+
+var compression = require('compression')
+var express = require('express');
+//var browserify = require('browserify');
+//var React = require('react');
+//var ReactDOM = require('react-dom/server');
+
+//var jsx = require('node-jsx');
+
+
+var app = express();
+
+
+//jsx.install();
+
+var Home = require('./src/home.jsx');
+
+app.use(compression());
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.end(
+
+  	ReactDOM.renderToStaticMarkup(
+  		React.createElement(Home)
+  	)
+
+  );
+});
+
+
+var server = app.listen(3000, function() {
+	var addr = server.address();
+	console.log('Listening @ http://%s:%d', addr.address, addr.port);
+})
