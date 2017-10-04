fe51cc6eeb981e7ba7517ab879bed9b87d0798f0
chenux <netares@gmail.com>
2017-10-04 18:07:26 -0500

    Update admin.js

--- views/admin.js
+++ views/admin.js
@@ -4,7 +4,7 @@
 var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
 	lineNumbers: true,
 	mode: 'text/x-c++src',
-	theme: 'monokai',
+	theme: 'mdn-like',
 	styleActiveLine: true,
 	matchBrackets: true,
 	indentUnit: 4,
